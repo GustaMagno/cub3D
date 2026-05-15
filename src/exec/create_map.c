53d2b2b2db6284f1a1dca24ab9c4ab060bccec12
Gustavo <gumagno2005@gmail.com>
2026-05-15 16:12:29 +0100

    wall color update

--- src/exec/create_map.c
+++ src/exec/create_map.c
@@ -73,7 +73,7 @@ void	put_wall(t_mlx *mlx, int x, int wall_color)
 		draw_start = 0;
 	if (draw_end >= screen_h)
 		draw_end = screen_h - 1;
-	wall_color = (0xAA0000 * mlx->ray->side) + (0xFF0000 * (mlx->ray->side == 0));
+	wall_color = (0x8B8B8B * mlx->ray->side) + (0x5A5A5A * (mlx->ray->side == 0));
 	while (++y < screen_h)
 	{
 		if (y < draw_start)
